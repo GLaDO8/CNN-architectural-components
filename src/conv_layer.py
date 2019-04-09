7fb0cabba1802d8cd1857e4a73a61182cd4e1a67
XshreyasguptaX <shreyas.charming@gmail.com>
2019-04-09 11:34:04 +0530

    optimisations

--- src/conv_layer.py
+++ src/conv_layer.py
@@ -2,7 +2,7 @@ import numpy as np
 
 #kernel_size is the flat2d size of the kernel. No need to pass the number of kernel layers. Eg - (3x3), (11x11) so pass 3, 11
 def imageToColumn(inp_layer, kernel_size, stride = 1, padding = 0):
-    H, W, C = inp_layer.shape[1], inp_layer.shape[0], inp_layer.shape[2]
+    C, H, W  = inp_layer.shape[0], inp_layer.shape[1], inp_layer.shape[2]
     out_height = int((H + 2*padding - kernel_size)/stride) + 1
     out_width = int((W + 2*padding - kernel_size)/stride) + 1
 
@@ -16,16 +16,19 @@ def imageToColumn(inp_layer, kernel_size, stride = 1, padding = 0):
     row_index = rowm.reshape(1, -1) + rowi.reshape(-1, 1)
 
     #image_2_col generation
-    inp_col = np.concatenate((np.vsplit(inp_layer[:, row_index, col_index], C)), axis = 2)
-    return inp_col
-
-def kernel_initialise(kernel_size):
-    Knum, Kdepth, Kx, Ky = kernel_size
-    return np.random.randint(2, size = (Knum, Kdepth, Kx, Ky))
+    inp_col = np.transpose(np.squeeze(np.concatenate((np.vsplit(inp_layer[:, row_index, col_index], C)), axis = 2)))
+    return inp_col, out_height, out_width
 
 #kernel is to be passed with the following shape params - (Knum, Kdepth, Kx, Ky)
-def kernelToRow(inp_layer, kernel, stride = 1):
+def kernelToRow(inp_layer, kernel, out_height, out_width, stride = 1):
     Knum, Kdepth, Kx, Ky = kernel.shape[0], kernel.shape[1], kernel.shape[2], kernel.shape[3] 
     kernel.reshape(-1, Kdepth*Kx*Ky)
     conv_out = np.matmul(kernel, inp_layer)/(Kx*Ky)
+    conv_out = conv_out.reshape(-1, out_height, out_width)
     return conv_out
+
+if __name__ == "__main__":
+    inp_layer = np.random.randint(3, size=(2, 4, 4))
+    kernel = np.random.randint(2, size = (4, 2, 3, 3))
+    image_col, out_height, out_width = imageToColumn(inp_layer, kernel.shape[2])
+    conv_out = kernelToRow(image_col, kernel, out_height, out_width)
